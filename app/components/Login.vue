70d4be697516103518e3fb144985108c6ff934bc
Rick Slingerland <kotorem.sama@gmail.com>
2021-11-24 13:37:07 +0100

    Login pagina klaar (nu nog account aanvragen)

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,23 +1,21 @@
 <template>
   <GridLayout rows="4*, 1*, 8*" height="100%">
-    <StackLayout row="0" marginTop="50">
-      <Label text="Inloggen" textAlignment="center" fontSize="32"></Label>
+    <StackLayout row="0" marginTop="30">
+      <Label class="logintext" text="Inloggen" fontSize=54em></Label>
+    </StackLayout>
+    <StackLayout row = "1">
+      <Label class="badlogin" ref="badlog" text="Deze inloggegevens zijn niet geldig. Probeer het opnieuw."></Label>
     </StackLayout>
     <StackLayout row="2">
       <StackLayout marginBottom="10">
-        <TextField ref="Naam" hint="Naam of e-mailadres"></TextField>
-        <TextField ref="Wachtwoord" hint="Wachtwoord" secure="true"></TextField>
+        <TextField class="nom" ref="Naam" hint="Naam of e-mailadres"></TextField>
+        <TextField class="nom" ref="Wachtwoord" hint="Wachtwoord" secure="true"></TextField>
       </StackLayout>
-      <Button class="-outline -rounded-sm" borderColor="3EC0FF" text="Login" padding="5" @tap="goToHome"></Button>
+      <Button class="loginbutton" text="Inloggen" @tap="goToHome"></Button>
       <Label textAlignment="center">
         <FormattedString>
           <Span text="Geen account? Klik dan "/>
-          <Span 
-            text="hier." 
-            color="blue"
-            textDecoration="underline"
-            fontWeight="bold"
-          />
+          <Span class="hiertext" text="hier"/>
         </FormattedString>
       </Label>
     </StackLayout>
@@ -53,6 +51,7 @@
   import Vue from "nativescript-vue";
   import { Component, Prop } from "vue-property-decorator";
   import newPerson from "@/Models/newPerson";
+  import "./Loginstyle.css";
 
   let users = [new newPerson("Rikkumoist",
       "https://yt3.ggpht.com/OHpZx8wQoQZiu45LMfcSKvDBO6gfR5_1ro_ZbS3xVpcRIu4Zqy_uHoWKpEdxTUD_Spq6zck0=s900-c-k-c0x00ffffff-no-rj",
@@ -65,22 +64,25 @@
 
   export default class Login extends Vue {
     msg: string = "Login";
+
     goToHome(args: TapGestureEventData) {
       let button: Button = args.object as Button;
-      button.borderColor = new Color("red");
       let gebruikersnaam: TextField = (this.$refs.Naam as any).nativeView as TextField;
       let wachtwoord: TextField = (this.$refs.Wachtwoord as any).nativeView as TextField;
-      let found = false;
+
       for (var index in users){
         if ((users[index].username.toLowerCase() == gebruikersnaam.text.toLowerCase() || users[index].email.toLowerCase() == gebruikersnaam.text.toLowerCase()) && users[index].password == wachtwoord.text){
           this.$emit("onLogin");
         }
         else{
-
           wachtwoord.text = "";
         }
       }
+      gebruikersnaam.className = "WrongG";
+      wachtwoord.className = "WrongG";
+      let bl = (this.$refs.badlog as any).nativeView;
+      bl.className = "badlogin2";
     }
-
   }
 </script>
+
