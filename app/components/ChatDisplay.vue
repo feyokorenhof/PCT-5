f02e72ab50edf1c8065c88ffebc882cbeace6f10
thrytas <victorroskam@gmail.com>
2021-12-22 13:50:59 +0100

    quick chat message commit

--- app/components/ChatDisplay.vue
+++ app/components/ChatDisplay.vue
@@ -1,21 +1,47 @@
 <template>
-    <Page actionBarHidden="true">
-        <!-- absolute layout for custom actiobar -->
-        <AbsoluteLayout>
-            <ScrollView width="100%" height="100%" marginTop="6%" marginBottom="5%">
-                <StackLayout>
-                    <label height="80" class="chat-container" />
-                    <label height="80" text:`${otherChatID}` class="chat-container" />
-                </StackLayout>
-            </ScrollView>
-            <GridLayout rows="2*, 12*, *" height="100%" width="100%">
-                <ActionBarTop row="0"/>
+  <Page actionBarHidden="true">
+    <!-- absolute layout for custom actiobar -->
+    <AbsoluteLayout>
+      <ScrollView width="100%" height="100%" marginTop="10%" marginBottom="5%">
+        <StackLayout>
+          <GridLayout v-for="msg in chat.messages" :key="msg.message_id">
+            <GridLayout v-if="isSender(msg)" columns="*, auto">
+              <StackLayout
+                orientation="horizontal"
+                class="message right"
+                col="1"
+                @tap="onMessageTap($event, msg)"
+              >
+                <Label :text="`${msg.text}`" textWrap="true" />
+              </StackLayout>
             </GridLayout>
-            <GridLayout columns="60, 410, 60" rows="50, 720, 60">
-                <Image src="~/Images/back_btn.png" row=0 col=0 marginTop=10 @tap="goBack"></Image>
+            <GridLayout v-if="!isSender(msg)" columns="auto, *">
+              <StackLayout
+                orientation="horizontal"
+                class="message left"
+                col="0"
+                @tap="onMessageTap($event, msg)"
+              >
+                <Label :text="`${msg.text}`" textWrap="true" />
+              </StackLayout>
             </GridLayout>
-        </AbsoluteLayout>
-    </Page>
+          </GridLayout>
+        </StackLayout>
+      </ScrollView>
+      <GridLayout rows="2*, 12*, *" height="100%" width="100%">
+        <ActionBarTop row="0" />
+      </GridLayout>
+      <GridLayout columns="60, 410, 60" rows="50, 720, 60">
+        <Image
+          src="~/Images/back_btn.png"
+          row="0"
+          col="0"
+          marginTop="10"
+          @tap="goBack"
+        ></Image>
+      </GridLayout>
+    </AbsoluteLayout>
+  </Page>
 </template>
 
 <script lang="ts">
@@ -24,10 +50,9 @@ import ActionBarTop from "./ActionBars/ActionBarTop.vue";
 import ActionBarBottom from "./ActionBars/ActionBarBottom.vue";
 import { Component, Prop } from "vue-property-decorator";
 import { GridLayout, TapGestureEventData } from "@nativescript/core";
-
 import { Screen } from "@nativescript/core/platform";
 import Message from "@/Models/Message";
-
+import Chat from "@/Models/Chat";
 @Component({
   name: "ChatDisplay",
   components: {
@@ -35,19 +60,25 @@ import Message from "@/Models/Message";
     ActionBarBottom
   }
 })
-
 export default class ChatDisplay extends Vue {
-    goBack() {
-        if (this.$modal) this.$modal.close();
-    }
-
-    @Prop() receiver_id!: string;
-
-    beforeMount() {
-        console.log(`receiver_id: ${this.receiver_id}`);
-    }
-};
-
+  @Prop() chat!: Chat;
+  get GetSH() {
+    return Screen.mainScreen.heightDIPs;
+  }
+  isSender(msg: Message) {
+    console.log(msg.sender_id == this.chat.sender_id);
+    return msg.sender_id == this.chat.sender_id;
+  }
+  beforeMount() {
+    console.log(`Loaded chat with id: ${this.chat.chat_id}`);
+  }
+  onMessageTap(args: TapGestureEventData, msg: Message) {
+    console.log(msg.message_id);
+  }
+  goBack() {
+    if (this.$modal) this.$modal.close();
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -57,7 +88,20 @@ export default class ChatDisplay extends Vue {
   //background-color: #FFFFFF; // slightlylighter
   //background-color: rgb(102, 101, 101); // og
   //background-color: rgb(61, 60, 60); // instagram
+  padding: 5;
+}
+.message {
+  color: white;
+  border-radius: 20;
+  padding: 5 10;
   margin-bottom: 5;
-  padding: 10;
+}
+.right {
+  background-color: #CD1045;
+  text-align: right;
+  float: right;
+}
+.left {
+  background-color: #615a5c;
 }
 </style>
--- app/components/Chats.vue
+++ app/components/Chats.vue
@@ -13,43 +13,52 @@
             :key="chat.chat_id"
             columns="3*, 2*, 4*, 4*, 2*"
             class="chat-container"
-            @tap="goChat(chat.receiver_id)"
-            >
-              <!-- profile picture -->
-              <GridLayout row="0" rowSpan="2" col="0">
-                <Image :src="chat.pfp_url" class="chat-profile-pic"></Image>
-              </GridLayout>
-              <!-- username/last message -->
-              <GridLayout row="0" col="1" colSpan="3" >
-                <Label textWrap="true" class="chat-text" ref="chatText" >
-                  <FormattedString>
-                    <Span :text="`${format(chat.username)} \n`" fontWeight="bold"></Span>
-                    <Span :text="format(chat.last_message)"></Span>
-                  </FormattedString>
-                </Label>
-              </GridLayout>
-              <!-- time last message received/sent -->
-              <GridLayout row="1" col="5">
-                <Label
-                  :text="chat.message_time"
-                  class="chat-time-passed"
-                  textWrap="true"
-                ></Label>
-              </GridLayout>    
-            </Gridlayout>
-            <!-- end chats with ender so chats don't fall behind bottom actionbar /-->
+            @tap="goChat($event, chat)"
+          >
+            <!-- profile picture -->
+            <GridLayout row="0" rowSpan="2" col="0">
+              <Image :src="chat.pfp_url" class="chat-profile-pic"></Image>
+            </GridLayout>
+            <!-- username/last message -->
+            <GridLayout row="0" col="1" colSpan="3">
+              <Label textWrap="true" class="chat-text" ref="chatText">
+                <FormattedString>
+                  <Span
+                    :text="`${format(chat.username)} \n`"
+                    fontWeight="bold"
+                  ></Span>
+                  <Span :text="format(chat.last_message)"></Span>
+                </FormattedString>
+              </Label>
+            </GridLayout>
+            <!-- time last message received/sent -->
+            <GridLayout row="1" col="5">
+              <Label
+                :text="chat.message_time"
+                class="chat-time-passed"
+                textWrap="true"
+              ></Label>
+            </GridLayout>
+          </Gridlayout>
+          <!-- end chats with ender so chats don't fall behind bottom actionbar /-->
           <label height="180" class="chat-container" />
         </StackLayout>
       </ScrollView>
       <!-- load in actiobar top and bottom in absolute layout -->
-    <GridLayout rows="2*, 12*, *" height="100%" width="100%">
-      <ActionBarTop row="0"/>
-      <ActionBarBottom row="2"/>
-    </GridLayout>
-    <GridLayout columns="60, 410, 60" rows="50, 720, 60">
-        <Image src="~/Images/back_btn.png" row=0 col=0 marginTop=10 @tap="goBack"></Image>
-        <Image src="~/Images/add_btn.png" row=3 col=3></Image>
-    </GridLayout>
+      <GridLayout rows="2*, 12*, *" height="100%" width="100%">
+        <ActionBarTop row="0" />
+        <ActionBarBottom row="2" />
+      </GridLayout>
+      <GridLayout columns="60, 410, 60" rows="50, 720, 60">
+        <Image
+          src="~/Images/back_btn.png"
+          row="0"
+          col="0"
+          marginTop="10"
+          @tap="goBack"
+        ></Image>
+        <Image src="~/Images/add_btn.png" row="3" col="3"></Image>
+      </GridLayout>
     </AbsoluteLayout>
   </Page>
 </template>
@@ -60,41 +69,34 @@ import { Component, Prop } from "vue-property-decorator";
 import { TapGestureEventData } from "@nativescript/core";
 import Chat from "@/Models/Chat";
 import User from "@/Models/User";
-
 import ActionBarTop from "./ActionBars/ActionBarTop.vue";
 import ActionBarBottom from "./ActionBars/ActionBarBottom.vue";
 //import Home from "./Home.vue";
-import ChatDisplay from "./ChatDisplay.vue"
-
+import ChatDisplay from "./ChatDisplay.vue";
 @Component({
   name: "Chats",
   components: {
     ActionBarTop,
-    ActionBarBottom,
-    //Home
+    ActionBarBottom
   }
 })
-
-
 export default class Chats extends Vue {
   msg: string = "Chats";
   max_char: number = 30;
   // set max character size in string, and when it exceeds max_char it truncates it with ellipsis.
   format(txt: string) {
-    if (txt.length > this.max_char)
-        return `${txt.substr(0, this.max_char)}...`;
+    if (txt.length > this.max_char) return `${txt.substr(0, this.max_char)}...`;
     return txt;
-  }  
+  }
   // function to go back to main screen
-  // goHome(){
-  //   this.$showModal(Home, {fullscreen: true})
-  // }
   goBack() {
-      //Back to Posts page.
-      if (this.$modal) this.$modal.close();}
-
-  goChat(receiver_id: String){
-    this.$showModal(ChatDisplay, {fullscreen: true, props: {receiver_id}})
+    if (this.$modal) this.$modal.close();
+  }
+  goChat(args: TapGestureEventData, chat: Chat) {
+    this.$showModal(ChatDisplay, {
+      fullscreen: true,
+      props: { chat: chat }
+    });
   }
   // chats aanmaken voor template, later inladen via API
   chats = [
@@ -103,10 +105,11 @@ export default class Chats extends Vue {
       sender_id: "1",
       receiver_id: "2",
       username: "fkorrie",
-      pfp_url: "https://cdn.vox-cdn.com/thumbor/VVXayrypyYIMqiHWIYdL77FRF_o=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/22408516/Big_Chungus.png",
+      pfp_url:
+        "https://cdn.vox-cdn.com/thumbor/VVXayrypyYIMqiHWIYdL77FRF_o=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/22408516/Big_Chungus.png",
       last_message: "Over het laatste nieuws gesproken",
       message_time: "18:46",
-      messages:[
+      messages: [
         {
           type: 5,
           text: "Sup biggas!",
@@ -116,10 +119,10 @@ export default class Chats extends Vue {
         },
         {
           type: 5,
-          text: "Lorem ipsum.",
+          text: "Sup foo.",
           message_id: "2",
-          sender_id: "1",
-          receiver_id: "2"
+          sender_id: "2",
+          receiver_id: "1"
         },
         {
           type: 5,
@@ -135,17 +138,27 @@ export default class Chats extends Vue {
       sender_id: "3",
       receiver_id: "2",
       username: "Anonymous",
-      pfp_url: "https://stickermaster.nl/30784-large_default/anonymous-sticker.jpg",
-      last_message: "Ik weet waar je huis woont, maar je huis weet niet waar ik woon. Of wel soms?",
-      message_time: "Gisteren"
-
+      pfp_url:
+        "https://stickermaster.nl/30784-large_default/anonymous-sticker.jpg",
+      last_message:
+        "Ik weet waar je huis woont, maar je huis weet niet waar ik woon. Of wel soms?",
+      message_time: "Gisteren",
+      messages: [
+        {
+          type: 5,
+          text:
+            "Ik weet waar je huis woont, maar je huis weet niet waar ik woon. Of wel soms?",
+          message_id: "1",
+          sender_id: "3",
+          receiver_id: "2"
+        }
+      ]
     }
-  ]
+  ];
 }
 </script>
 
 <style lang="scss" scoped>
-
 .chat-container {
   border-top-width: 2px;
   border-color: #757575;
@@ -155,7 +168,6 @@ export default class Chats extends Vue {
   margin-bottom: 5;
   padding: 10;
 }
-
 .chat-profile-pic {
   width: 80;
   height: 80;
@@ -163,20 +175,17 @@ export default class Chats extends Vue {
   border-width: 1;
   border-color: #757575;
   object-fit: scale-down;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 }
-
 .chat-text {
   color: black;
   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
     "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
   font-size: 22em;
 }
-
 .chat-time-passed {
   color: black;
   font-style: italic;
   font-size: 18em;
 }
-
 </style>
