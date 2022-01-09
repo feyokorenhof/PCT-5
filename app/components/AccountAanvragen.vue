9f693901e9b351af9975c5b1768edd6e8033ad5e
Rick Slingerland <kotorem.sama@gmail.com>
2022-01-09 14:32:39 +0100

    e

--- app/components/AccountAanvragen.vue
+++ app/components/AccountAanvragen.vue
@@ -1,5 +1,6 @@
 <template>
-  <GridLayout rows="4*, 1*, 8*" height="100%">
+<AbsoluteLayout>
+  <GridLayout rows="4*, 1*, 8*" height="100%" width="100%">
     <Label marginTop="30" class="accaantext" text="Account Aanvragen" fontSize=25em></Label>
     <Label marginTop="85" class="verplicht" ref="badlog" text="De invulvakken met een ster '*' zijn verplicht"></Label>
     <TextField autocapitalizationType="none" class="nom" height="7%" marginTop="140" ref="Voornaam" hint="Voornaam *"></TextField>
@@ -7,7 +8,9 @@
     <TextField autocapitalizationType="none" class="nom" height="7%" marginTop="320" ref="Emailadres" hint="E-mailadres *"></TextField>
     <TextField autocapitalizationType="none" class="nom" height="7%" marginTop="410" ref="Bedrijfsnaam" hint="Bedrijfsnaam"></TextField>
     <Button class="accaanbutton" marginTop="560" text="Account Aanvragen" @tap="onLinkTap"></Button>
+    <TextField class="accountmaker" height="7%" marginTop="710" ref="accountmaker"></TextField>
   </GridLayout>
+</AbsoluteLayout>
 </template>
 
 <script lang="ts">
@@ -15,22 +18,24 @@
   import Vue from "nativescript-vue";
   import { Component, Prop } from "vue-property-decorator";
   import "./Loginstyle.css";
-  // import * as email from "nativescript-email";  
 
   @Component({ name: "AccountAanvragen", components: {}})
   
   export default class AccountAanvragen extends Vue {
     msg: string = "AccountAanvragen";
+
+    goBack(){
+
+    }
     
     onLinkTap(args: TapGestureEventData) {
-      const random = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-      console.log(random);
-      
+      const random = "1234" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + "4321";
       let button: Button = args.object as Button;
       let voornaam: TextField = (this.$refs.Voornaam as any).nativeView as TextField;
       let achternaam: TextField = (this.$refs.Achternaam as any).nativeView as TextField;
       let emailadres: TextField = (this.$refs.Emailadres as any).nativeView as TextField;
       let bedrijfsnaam: TextField = (this.$refs.Bedrijfsnaam as any).nativeView as TextField;
+      let AccCode: TextField = (this.$refs.accountmaker as any).nativeView as TextField;
       
       if (voornaam.text != ""){
         voornaam.className = "nom"
@@ -59,17 +64,8 @@
       }
       if (voornaam.text != "" && achternaam.text != "" && emailadres.text != ""){
         this.$emit("accountAangevraagd");
-
-        // email.available().then((avail: boolean) => {
-        //   console.log("Email available? " + avail);
-        // })
-        
-        // email.compose({
-        //   subject: "Yo",
-        //   body: "Hello <strong>dude</strong> :)",
-        //   to: ['rickyman2002.rick@gmail.com']
-        // })
       }
+        AccCode.text = random;
     }
   }
 </script>
